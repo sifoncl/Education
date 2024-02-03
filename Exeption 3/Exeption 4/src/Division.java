3da4e5a225784abfd014468edd744bb8d37e6b72
sifoncl <virusss1990@gmail.com>
2024-02-04 01:18:15 +0300

    sd

---
+++ Exeption 3/Exeption 4/src/Division.java
@@ -0,0 +1,29 @@
+import static java.lang.Integer.parseInt;
+
+public class Division {
+
+    public static Double divide(String... arguments) {
+
+        int args[] = new int[arguments.length];
+
+        for (int i = 0; i <= arguments.length - 1; i++) {
+            try {
+                args[i] = parseInt(arguments[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Символ(ы): " + arguments[i] + " не являются числом");
+                System.exit(0);
+            }
+        }
+        if (args[0] == 0) {
+            return (double) 0;
+        }
+
+        double ans = (double) args[0];
+
+        for (int i = 1; i <= args.length - 1; i++) {
+            ans /= args[i];
+            System.out.println(ans);
+        }
+        return ans;
+    }
+}
---
+++ Exeption 3/Exeption 4/src/Test.java
@@ -0,0 +1,7 @@
+import static java.lang.Integer.valueOf;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Division.divide("1", "b", "2", "4"));
+    }
+}
