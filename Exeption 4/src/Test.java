86844a5f30fc25a70004841f511d9a6eb8db0692
sifoncl <virusss1990@gmail.com>
2024-02-04 03:09:27 +0300

    sd

--- Exeption 4/src/Test.java
+++ Exeption 4/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Division.divide("1", "b", "2", "4"));
+        System.out.println(Division.divide("5000", "b", "2", "4"));
     }
 }
