c3b52cf3c3d6f9808272c960969fc8183618bac9
sifoncl <virusss1990@gmail.com>
2024-02-09 04:40:55 +0300

    sd

--- Time 1/src/Main.java
+++ Time 1/src/Main.java
@@ -1,5 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+        int months = scn.nextInt();
+
+        LocalDate date = LocalDate.of(2024, months, 1);
+
+        while (months == date.getMonthValue()) {
+            System.out.println(date.format(DateTimeFormatter.ofPattern("d::MMM::uuuu")));
+            date = date.plusDays(1);
+        }
+
     }
 }
--- Time 2/src/Main.java
+++ Time 2/src/Main.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scn = new Scanner(System.in);
+        int dayNum = scn.nextInt();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime someDaysBefore = now.minusDays(dayNum);
+
+        ZoneId yerevan = ZoneId.of("Asia/Yerevan");
+        someDaysBefore.atZone(yerevan);
+
+        System.out.println(someDaysBefore.format(DateTimeFormatter.ofPattern("d MMMM yyyy HH:mm")));
+
     }
 }
