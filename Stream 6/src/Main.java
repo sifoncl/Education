41bc8909418b4ad9f33829c160e946d98e65b5f3
Sifoncl <virusss1990@gmail.com>
2024-02-22 11:02:52 +0300

    Commit

--- Stream 6/src/Main.java
+++ Stream 6/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int[] nums = {1, 2, 3, 10, 12, 14, 19, 24, 25, 223233};
+
+        int first = Arrays.stream(nums).filter(x -> x >= 24).findFirst().getAsInt();
+
+        System.out.println(first);
+
     }
+
+
 }
