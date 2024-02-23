73de572a89899b44d050643f518534c84c308c9a
sifoncl <virusss1990@gmail.com>
2024-02-23 19:11:19 +0300

    sd

--- XML 1/src/Main.java
+++ XML 1/src/Main.java
@@ -1,5 +1,57 @@
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+
+        XMLInputFactory factory = XMLInputFactory.newFactory();
+        XMLStreamReader reader = factory.createXMLStreamReader(new
+                FileInputStream("XML 1/somePoints.xml"));
+
+        ArrayList<Integer[]> points = new ArrayList<Integer[]>();
+        String tagContent = null;
+        Integer[] point = new Integer[2];
+
+        while (reader.hasNext()) {
+
+
+            int event = reader.next();
+
+            switch (event) {
+                case XMLStreamConstants.START_ELEMENT:
+                    if (reader.getLocalName().equals("point")) {
+                        point = new Integer[2];
+                    }
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    tagContent = reader.getText().trim();
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    switch (reader.getLocalName()) {
+                        case "x":
+                            point[0] = Integer.valueOf(tagContent);
+                            break;
+                        case "y":
+                            point[1] = Integer.valueOf(tagContent);
+                            break;
+                        case "point":
+                            points.add(point);
+                            break;
+                    }
+
+            }
+
+        }
+        for (Integer[] p : points) {
+            System.out.println(p[0] + " " + p[1] + " px");
+        }
     }
+
 }
+
+
